b9c4050b2d0d36ed8bab689ae1fe85a7c97a7832
amadboc <aimadero@gmail.com>
2018-06-21 13:59:22 +0200

    cambios

---
+++ 01_Datos/Hola_Funcion.js
@@ -0,0 +1,17 @@
+/*
+    JavaScript (@Oracle) -> ECMAScript -> ES6 / ES2015
+*/
+
+function saludar(user) {
+    const SALUDO = 'Hola'
+    console.log(SALUDO, pUser)
+}
+
+function sumar(a, b) {
+    let resultado = a + b
+    console.log(resultado)
+}
+
+saludar('Pepe')
+saludar('Helena')
+sumar (2, 4)
---
+++ 01_Datos/Hola_Mundo.js
@@ -0,0 +1,9 @@
+//declaración
+var saludo
+//inicialización
+saludo = "Hola"
+
+//declaración + inicialización
+var user = "Mundo"
+
+console.log(saludo, user)
