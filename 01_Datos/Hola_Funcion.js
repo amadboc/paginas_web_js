96e5ef634a82877108702bca7b5936a8f709b689
amadboc <aimadero@gmail.com>
2018-06-22 09:31:49 +0200

    cambios 01_Datos

--- 01_Datos/Hola_Funcion.js
+++ 01_Datos/Hola_Funcion.js
@@ -1,17 +1,17 @@
 /*
-    JavaScript (@Oracle) -> ECMAScript -> ES6 / ES2015
-*/
+  JavaScript (@Oracle) -> ECMAScript -> ES6 / ES2015
+ */
 
-function saludar(user) {
+function saludar(pUser) {
     const SALUDO = 'Hola'
     console.log(SALUDO, pUser)
 }
 
-function sumar(a, b) {
+function kraus(a,b) {
     let resultado = a + b
     console.log(resultado)
 }
 
 saludar('Pepe')
-saludar('Helena')
-sumar (2, 4)
+saludar('Elena')
+kraus(2,4)
--- 01_Datos/Hola_Mundo.js
+++ 01_Datos/Hola_Mundo.js
@@ -4,6 +4,20 @@ var saludo
 saludo = "Hola"
 
 //declaración + inicialización
-var user = "Mundo"
+var user ='Mundo'
+var edad = 23
+var estudiante = false
+var algo
+//Objeto en notacion JSON
+var persona = {
+    nombre:'Pepe',
+    edad: 34,
+    isEstudiante: true,
+}
 
 console.log(saludo, user)
+console.log(nombre)
+console.log(edad)
+console.log(isEstudiante)
+console.log(persona)
+console.log(persona.nombre)
