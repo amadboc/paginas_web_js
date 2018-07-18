4766434866d7b282378573348640bbcbb94b173c
amadboc <aimadero@gmail.com>
2018-07-18 14:00:38 +0200

    cambios 06_constructores

--- 05_Objetos_propios/factura.2.js
+++ 05_Objetos_propios/factura.2.js
@@ -1,4 +1,29 @@
-//otra forma de hacer el ejercicio factura.js
+function Empresa (nombre, direccion, telefono, nif) {
+    this.nombre = nombre
+    this.direccion = direccion
+    this.telefono = telefono
+    this.nif = nif
+}
+
+function Elementos (descripcion, precio, cantidad) {
+    this.descripcion = descripcion
+    this.precio = precio
+    this.cantidad = cantidad
+}
+
+function Factura (empresa,cliente,elementos) {
+    this.empresa = ''
+    this.cliente = ''
+    this.elementos = []
+    this.importeTotal = 0
+    this.tipoIva = tipoIva
+    this.formaPago = formaPago
+}
+
+let factura1 = new Factura (1.04,)
+
+
+
 
 let factura1 = {}
     
---
+++ 06_Facturas_web/Factura.js
@@ -0,0 +1,7 @@
+function Factura (tipoIVA, formaPago) {
+    this.cliente = ''
+    this.elementos = []
+    this.importeTotal = 0
+    this.tipoIVA = tipoIVA
+    this.formaPago = formaPago
+}
