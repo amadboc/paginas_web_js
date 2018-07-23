17808e1128a11207928312b1f977eed986f2ce11
amadboc <aimadero@gmail.com>
2018-07-23 13:50:08 +0200

    cambios 10_Web

---
+++ 10_Web/Hola_Mundo2/app3.js
@@ -0,0 +1,40 @@
+class Apps{
+    constructor() {
+        this.amigo = 'Pepe'
+        this.oBtnSaludar = document.querySelector('#btnSaludar')
+        this.oBtnDespedir = document.querySelector('#btnDespedir') 
+        this.asignarHandlers()
+    }
+
+    asignarHandlers() {
+        console.log(this)
+        this.oBtnSaludar.addEventListener('click', this.saludar.bind(this))
+        this.oBtnDespedir.addEventListener('click', this.despedir.bind(this)) 
+    }
+
+    saludar(oE) {
+        console.log(oE.target);
+        console.log(this)
+        console.log(`Hola ${this.amigo}`)
+    }
+  
+    despedir() {
+        console.log(`Adios ${this.amigo}`)
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', 
+        () =>  new Apps() )
+
+// Seria lo mismo que 
+// document.addEventListener('DOMContentLoaded', 
+//        function () {new Apps()})
+
+
+// Sería lo mismo que 
+//function main() {
+//    new Apps()
+//}
+// document.addEventListener('DOMContentLoaded', main)
+
